b9c02103295de3ab7e83c67f5338f7df31d45e34
h0rhay <clarkgeorge76@gmail.com>
2019-03-20 18:46:50 +0000

    remove all pages and blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
+// This is for lambda functions
 var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Daedalus - Cryptocurrency Wallet',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'The extensible open source cryptocurrency wallet, built to grow with the community.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
